d9261adfcf45432a16824734881ac0f861d7fd0b
deefactorial <deefactorial@gmail.com>
2015-05-16 18:06:34 -0700

    updated nodemon to die gracefully

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,30 @@ var path = require('path');
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        "nodemon": {
+            "dev": {
+                "script": "index.js"
+            }
+        }
     });
 
-    grunt.registerTask('runNode', function () {
-        grunt.util.spawn({
-            cmd: 'node',
-            args: ['./node_modules/nodemon/nodemon.js', 'index.js'],
-            opts: {
-                stdio: 'inherit'
-            }
-        }, function () {
-            grunt.fail.fatal(new Error("nodemon quit"));
-        });
-    });
+    //grunt.registerTask('runNode', function () {
+    //    grunt.util.spawn({
+    //        cmd: 'node',
+    //        args: ['./node_modules/nodemon/nodemon.js', 'index.js'],
+    //        opts: {
+    //            stdio: 'inherit'
+    //        }
+    //    }, function () {
+    //        grunt.fail.fatal(new Error("nodemon quit"));
+    //    });
+    //});
+
+    grunt.loadNpmTasks('grunt-nodemon');
 
     // Run the server
-    grunt.registerTask('server', ['runNode']);
+    grunt.registerTask('server', ['nodemon']);
 
     // Default task(s).
     grunt.registerTask('default', ['server']);
