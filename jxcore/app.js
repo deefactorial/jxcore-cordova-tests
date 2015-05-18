afcecd8ed63cf25ae82c2c6db334b51a3e8367f4
deefactorial <deefactorial@gmail.com>
2015-05-18 14:46:52 -0700

    restructure

--- jxcore/app.js
+++ jxcore/app.js
@@ -1,1 +1,28 @@
-require("../app.js");
+cordova('asyncSeverStarted').registerAsync(function(message, callback){
+
+    console.log("asyncSeverStarted called:" + message);
+
+    if (message == "Hello") {
+
+        var app = require("./index.js");
+
+        callback("Pong:" + message);
+
+    } else if (message == "Hello supertest") {
+
+        var app = require("./supertest.js");
+
+        callback("Pong:" + message);
+
+    } else if (message == "Hello cookie-parser") {
+
+        var app = require("./cookie-parser.js");
+
+        callback("Pong:" + message);
+
+    } else {
+        callback("unknown message:" + message);
+    }
+
+});
+
