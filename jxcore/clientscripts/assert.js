aa8117249be80654658981d22b441f897bad9c81
deefactorial <deefactorial@gmail.com>
2015-05-20 12:36:04 -0700

    finish superagent tests

--- jxcore/clientscripts/assert.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert');
-if (typeof global.window.define == 'function' && global.window.define.amd) {
-    global.window.define('assert', function () { return assert; });
-} else {
-    global.window.assert = assert;
-}
