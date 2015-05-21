f18b51dc68716702cb1df52550082a3003605bde
deefactorial <deefactorial@gmail.com>
2015-05-21 10:39:57 -0700

    finish superagent tests

--- jxcore/test/express/app.all.js
+++ jxcore/test/express/app.all.js
@@ -1,35 +1,46 @@
+if(typeof window !== 'undefined') {
+  var express = require('express')
+      , request = require('supertest');
+  var app = express();
 
-var express = require('../')
-  , request = require('supertest');
+  app.all('/tobi', function(req, res){
+    res.end(req.method);
+  });
 
-describe('app.all()', function(){
-  it('should add a router per method', function(done){
-    var app = express();
+  var n = 0;
+  app.all('/*', function(req, res, next){
+    if (n++) return done(new Error('DELETE called several times'));
+    next();
+  });
 
-    app.all('/tobi', function(req, res){
-      res.end(req.method);
+  var server = app.listen(5001, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+  });
+}
+
+if(typeof describe !== 'undefined') {
+  var uri = "http://localhost:5001";
+  describe('app.all()', function () {
+    it('should add a router per method', function (done) {
+
+      request()
+          .put(uri + '/tobi')
+          .expect('PUT', function () {
+            request(app)
+                .get('/tobi')
+                .expect('GET', done);
+          });
     });
 
-    request(app)
-    .put('/tobi')
-    .expect('PUT', function(){
-      request(app)
-      .get('/tobi')
-      .expect('GET', done);
-    });
-  })
-
-  it('should run the callback for a method just once', function(done){
-    var app = express()
-      , n = 0;
-
-    app.all('/*', function(req, res, next){
-      if (n++) return done(new Error('DELETE called several times'));
-      next();
-    });
-
-    request(app)
-    .del('/tobi')
-    .expect(404, done);
-  })
-})
+    it('should run the callback for a method just once', function (done) {
+      request()
+          .del(uri + '/tobi')
+          .expect(404, done);
+    })
+  });
+}
