01ab6b4dfd6268f1e69de1f2b6a02c153e0c65b7
deefactorial <deefactorial@gmail.com>
2015-05-28 16:08:14 -0700

    cleanup tests to move over to jxcore

--- jxcore/Gruntfile.js
+++ jxcore/Gruntfile.js
@@ -4,29 +4,6 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        nodemon: {
-            dev: {
-                script: 'test/superagent-shared.js',
-                options: {
-                    args: ['dev'],
-                    nodeArgs: ['--debug'],
-                    callback: function (nodemon) {
-                        nodemon.on('log', function (event) {
-                            console.log(event.colour);
-                        });
-                    },
-                    env: {
-                        PORT: '8181'
-                    },
-                    cwd: __dirname,
-                    ignore: ['node_modules/**'],
-                    ext: 'js',
-                    watch: ['server'],
-                    delay: 1000,
-                    legacyWatch: true
-                }
-            }
-        },
         browserify: {
             dist: {
                 files: {
@@ -38,10 +15,8 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-browserify');
 
-    grunt.loadNpmTasks('grunt-nodemon');
-
     // Run the server
-    grunt.registerTask('server', ['browserify','nodemon']);
+    grunt.registerTask('server', ['browserify']);
 
     // Default task(s).
     grunt.registerTask('default', ['server']);
